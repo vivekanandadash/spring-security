8ec18cc1ff6e48ee66b93a915ac3249f84352a9b
Vivekananda <vivekanandadash245@gmail.com>
2026-02-13 01:02:55 +0530

    commit

--- src/main/java/com/auth/controller/WelcomeController.java
+++ src/main/java/com/auth/controller/WelcomeController.java
@@ -1,4 +1,18 @@
 package com.auth.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/welcome")
 public class WelcomeController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "hi";
+    }
 }
