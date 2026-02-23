f2616016ef6db712110a0ddf164f40788fc6a3bb
Vivekananda <vivekanandadash245@gmail.com>
2026-02-23 07:17:53 +0530

    Optimizing login features try catch block creation in controller layer

--- src/main/java/com/auth/controller/AuthController.java
+++ src/main/java/com/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,33 +32,35 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<APIResponse<String>> createUser(
             @RequestBody UserDto userDto
-            ){
+    ) {
         APIResponse<String> response = authService.register(userDto);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
 
     }
+
     @PostMapping("/login")
     public ResponseEntity<APIResponse<String>> loginCheck(
             @RequestBody LoginDto loginDto
-            ){
+    ) {
         APIResponse<String> response = new APIResponse<>();
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDto.getUsername(),loginDto.getPassword());
-        Authentication authenticate = authManager.authenticate(token);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
 
-        System.out.println(authenticate.isAuthenticated());
-        if (authenticate.isAuthenticated()){
+        try {
+            Authentication authenticate = authManager.authenticate(token);
+            System.out.println(authenticate.isAuthenticated());
             response.setMessage("Login Successfully");
             response.setStatus(200);
             response.setData("Transaction Completed");
-            return new ResponseEntity<>(response,HttpStatus.OK);
-        }
-        else{
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            System.out.println("login failed :" + e.getMessage());
             response.setMessage("Check Your Credential");
             response.setStatus(404);
             response.setData("User Not Found");
-            return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
 
-        }
+
+    }
 
 }
